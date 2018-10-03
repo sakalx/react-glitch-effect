180f11768cc2645d10163262a3985c3eaa749f22
sakal <sakals@mail.ua>
2018-10-03 17:28:09 -0400

    init: example page
    Signed-off-by: sakal <sakals@mail.ua>

--- example/src/index.js
+++ example/src/index.js
@@ -3,35 +3,92 @@ import {render} from 'react-dom';
 import GlitchEffect from '../../src';
 
 class App extends React.Component {
-
   state = {
-    disabled: true,
+    disabled: false,
   };
 
   handleToggleGlitch = () => {
     this.setState(state => ({disabled: !state.disabled}))
   };
 
-
   render() {
     const {disabled} = this.state;
 
     return (
-      <div>
-        <GlitchEffect  disabled={false} style={{background: 'green'}}>
-          <div>
-            <h1>Hello from My Component</h1><h1>Hello from My Component</h1><h1>Hello from My Component</h1><h1>Hello from My Component</h1><h1>Hello from My Component</h1>
-            <img src={}/>
-          </div>
-        </GlitchEffect>
-        <GlitchEffect duration="5s" disabled={false} className="foo">
-          <h2 align="center">Foo Bar Baz</h2>
+      <div style={styles.wrap}>
+        <GlitchEffect disabled={disabled} style={styles.wallpaper}>
+          <img style={styles.wallpaper__img}
+               src={'https://raw.githubusercontent.com/sakalx/react-glitch-effect/master/static/img/store-1245758_1920.jpg'}/>
         </GlitchEffect>
 
-        <button onClick={this.handleToggleGlitch}>Toggle</button>
+        <button style={styles.button__toggle}
+                onClick={this.handleToggleGlitch}
+        >
+          Toggle
+        </button>
+
+        <GlitchEffect disabled={disabled} duration='1s' style={styles.title}>
+          <h1>Glitch Effect</h1>
+        </GlitchEffect>
+
+        <GlitchEffect onHover={true}>
+          <figure>
+            <figcaption style={styles.portrait__title}>
+              Hover me
+            </figcaption>
+            <img style={styles.portrait__img}
+                 src={'https://raw.githubusercontent.com/sakalx/react-glitch-effect/master/static/img/oldtimer-1245746_1920.jpg'}/>
+          </figure>
+        </GlitchEffect>
       </div>
     )
   }
 }
+
+const styles = {
+  wrap: {
+    alignItems: 'flex-start',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  wallpaper: {
+    position: 'absolute',
+    zIndex: -1,
+  },
+  wallpaper__img: {
+    height: '100vh',
+    width: '100vw',
+  },
+  title: {
+    alignSelf: 'center',
+    fontSize: 34,
+  },
+  portrait__img: {
+    border: '5px solid #fff',
+    borderRadius: '3%',
+    cursor: 'pointer',
+    height: 'auto',
+    width: '25vw',
+  },
+  portrait__title: {
+    fontSize: 34,
+    fontWeight: 500,
+    textShadow: '-1px -1px 1px rgba(255,255,255,.1), 1px 1px 1px rgba(0,0,0,.5)',
+  },
+  button__toggle: {
+    alignSelf: 'flex-end',
+    background: 'rgba(255,255, 255, 0)',
+    borderRadius: 8,
+    color: '#fff',
+    cursor: 'pointer',
+    fontSize: 24,
+    margin: 15,
+    outline: 'none',
+    padding: 2,
+    textShadow: '-1px -1px 1px rgba(255,255,255,.1), 1px 1px 1px rgba(0,0,0,.5)',
+    width: 125,
+  },
+
+};
 
 render(<App/>, document.getElementById("root"));
