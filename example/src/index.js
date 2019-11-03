854edf7710f7d34bbc809222c4645a86f68feacc
sakal <sakals@mail.ua>
2019-11-02 23:18:23 -0400

    Chore: Updated Example page.
    Signed-off-by: sakal <sakals@mail.ua>

--- example/src/index.js
+++ example/src/index.js
@@ -1,88 +1,63 @@
 import React, {useState} from 'react';
 import {render} from 'react-dom';
 import GlitchEffect from '../../src';
+import GlitchText from '../../src/Text';
+
+import style from './style';
 
 const App = () => {
-  const [isDisabled, setDisabled] = useState(false);
+  const [isDisabledGlitch, setDisabledGlitch] = useState({
+    main: false,
+    text: false,
+  });
 
-  const handleToggleGlitch = () => {
-    setDisabled(!isDisabled);
+  const handleToggleGlitch = name => () => {
+    setDisabledGlitch(pre => ({...pre, [name]: !isDisabledGlitch[name]}));
   };
-
+//
   return (
-      <div style={styles.wrap}>
-        <GlitchEffect disabled={isDisabled} style={styles.wallpaper}>
-          <img style={styles.wallpaper__img}
+      <main style={style.wrap}>
+        <GlitchEffect disabled={isDisabledGlitch.main} style={style.wallpaper}>
+          <img style={style.wallpaper__img}
                alt='Background image with glitch effect'
-               src={'https://raw.githubusercontent.com/sakalx/react-glitch-effect/master/static/img/store-1245758_1920.jpg'}/>
+               src='https://i.ibb.co/M8RHHBN/main.jpg'/>
         </GlitchEffect>
 
-        <button style={styles.button__toggle} onClick={handleToggleGlitch}>
-          Toggle
-        </button>
+        <div style={style.row}>
+          <section>
+            <button style={style.button__toggle} onClick={handleToggleGlitch('text')}>
+              TOGGLE TEXT GLITCH
+            </button>
+            <GlitchText disabled={isDisabledGlitch.text} component='h1' style={style.title}>
+              Text Glitch
+            </GlitchText>
+          </section>
 
-        <GlitchEffect disabled={isDisabled} duration='1s' style={styles.title}>
-          <h1>Glitch Effect</h1>
+          <button style={style.button__toggle} onClick={handleToggleGlitch('main')}>
+            TOGGLE MAIN GLITCH
+          </button>
+        </div>
+        <GlitchEffect disabled={isDisabledGlitch.main} duration='1s' style={style.title}>
+          <h1 style={{margin: 24}}>Glitch</h1>
         </GlitchEffect>
 
-        <GlitchEffect onHover={true}>
-          <figure>
-            <figcaption style={styles.portrait__title}>
-              Hover me
-            </figcaption>
-            <img style={styles.portrait__img}
-                 alt='Image with glitch effect when hoover'
-                 src={'https://raw.githubusercontent.com/sakalx/react-glitch-effect/master/static/img/oldtimer-1245746_1920.jpg'}/>
-          </figure>
-        </GlitchEffect>
-      </div>
+        <section style={style.row}>
+          <GlitchText onHover={true} component='h5' style={style.title}>
+            Hover this text
+          </GlitchText>
+          <GlitchEffect onHover={true}>
+            <figure>
+              <figcaption style={style.portrait__title}>
+                Hover me
+              </figcaption>
+              <img style={style.portrait__img}
+                   alt='Image with glitch effect when hoover'
+                   src='https://i.ibb.co/MRKBztH/secondary.jpg'/>
+            </figure>
+          </GlitchEffect>
+        </section>
+      </main>
   );
 };
-
-const styles = {
-  wrap: {
-    alignItems: 'flex-start',
-    display: 'flex',
-    flexDirection: 'column',
-  },
-  wallpaper: {
-    position: 'absolute',
-    zIndex: -1,
-  },
-  wallpaper__img: {
-    height: '100vh',
-    width: '100vw',
-  },
-  title: {
-    alignSelf: 'center',
-    fontSize: 34,
-  },
-  portrait__img: {
-    border: '5px solid #fff',
-    borderRadius: '3%',
-    cursor: 'pointer',
-    height: 'auto',
-    width: '25vw',
-  },
-  portrait__title: {
-    fontSize: 34,
-    fontWeight: 500,
-    textShadow: '-1px -1px 1px rgba(255,255,255,.1), 1px 1px 1px rgba(0,0,0,.5)',
-  },
-  button__toggle: {
-    alignSelf: 'flex-end',
-    background: 'rgba(255,255, 255, 0)',
-    borderRadius: 8,
-    color: '#fff',
-    cursor: 'pointer',
-    fontSize: 24,
-    margin: 15,
-    outline: 'none',
-    padding: 2,
-    textShadow: '-1px -1px 1px rgba(255,255,255,.1), 1px 1px 1px rgba(0,0,0,.5)',
-    width: 125,
-  },
-
-};
-
+// https://raw.githubusercontent.com/sakalx/react-glitch-effect/master/static/img/secondary.jpg
 render(<App/>, document.getElementById('root'));
---
+++ example/src/style.js
@@ -0,0 +1,51 @@
+export default {
+  wrap: {
+    alignItems: 'flex-start',
+    display: 'flex',
+    flexDirection: 'column',
+  },
+  row: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    padding: '0 16px',
+    width: '97%',
+  },
+  wallpaper: {
+    position: 'absolute',
+    zIndex: -1,
+  },
+  wallpaper__img: {
+    height: '100vh',
+    width: '98.5vw',
+  },
+  title: {
+    alignSelf: 'center',
+    fontSize: 34,
+    margin: 0,
+  },
+  portrait__img: {
+    border: '5px solid #fff',
+    borderRadius: '3%',
+    cursor: 'pointer',
+    height: '200px',
+    width: '160px',
+  },
+  portrait__title: {
+    fontSize: 34,
+    fontWeight: 500,
+    textShadow: '-1px -1px 1px rgba(255,255,255,.1), 1px 1px 1px rgba(0,0,0,.5)',
+  },
+  button__toggle: {
+    letterSpacing: 2,
+    background: 'rgba(0,0, 0, .6)',
+    borderRadius: 8,
+    color: '#fff',
+    cursor: 'pointer',
+    fontSize: 14,
+    margin: 12,
+    outline: 'none',
+    padding: 6,
+    textShadow: '-1px -1px 1px rgba(255,255,255,.1), 1px 1px 1px rgba(0,0,0,.5)',
+    width: 135,
+  },
+};
