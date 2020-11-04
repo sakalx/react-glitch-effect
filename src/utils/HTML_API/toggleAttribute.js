18e6229b847d1ab1089edc53b2e7b6325e1e8d5c
serhii.sakal <serhii.sakal@axoni.com>
2020-11-03 20:52:48 -0500

    HTML API utils
    Signed-off-by: serhii.sakal <serhii.sakal@axoni.com>

---
+++ src/utils/HTML_API/toggleAttribute.js
@@ -0,0 +1,13 @@
+import checkType from "../generic/checkType";
+
+const isValidValue = checkType(['string']);
+
+export const addAttribute = qualifiedName => elementRef => value => {
+    if (isValidValue(value)) elementRef.current?.setAttribute(qualifiedName, value);
+    return elementRef;
+}
+
+export const removeAttribute = qualifiedName => elementRef => () => {
+    if (isValidValue(value)) elementRef.current?.removeAttribute(qualifiedName,);
+    return elementRef;
+}
