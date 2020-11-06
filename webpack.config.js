e7cfb53e1a78cf595cf2e8bc453a5af20e2b2dc6
serhii.sakal <serhii.sakal@axoni.com>
2020-11-06 16:46:49 -0500

    airbnb eslint config
    Signed-off-by: serhii.sakal <serhii.sakal@axoni.com>

--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,43 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const htmlWebpackPlugin = new HtmlWebpackPlugin({
-	template: path.join(__dirname, 'example/src/index.html'),
-	filename: './index.html',
+  template: path.join(__dirname, 'example/src/index.html'),
+  filename: './index.html',
 });
 
 module.exports = {
-	devtool: 'cheap-source-map',
+  devtool: 'cheap-source-map',
 
-	entry: path.join(__dirname, 'example/src/index.js'),
+  entry: path.join(__dirname, 'example/src/index.js'),
 
-	output: {
-		path: path.join(__dirname, 'example/dist'),
-		filename: 'bundle.js',
-	},
+  output: {
+    path: path.join(__dirname, 'example/dist'),
+    filename: 'bundle.js',
+  },
 
-	module: {
-		rules: [
-			{
-				test: /\.(js|jsx)$/,
-				use: 'babel-loader',
-				exclude: /node_modules/,
-			},
-			{
-				test: /\.css$/,
-				use: ['style-loader', 'css-loader'],
-			},
-		],
-	},
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
 
-	resolve: {
-		extensions: ['.js', '.jsx'],
-		alias: {
-			root: path.resolve(__dirname, 'src'),
-			core: path.resolve(__dirname, 'src/__core__'),
-		},
-	},
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
 
-	devServer: {
-		port: 3000,
-		open: true,
-		inline: true,
-	},
+  devServer: {
+    port: 3000,
+    open: true,
+    inline: true,
+  },
 
-	plugins: [htmlWebpackPlugin],
+  plugins: [htmlWebpackPlugin],
 };
