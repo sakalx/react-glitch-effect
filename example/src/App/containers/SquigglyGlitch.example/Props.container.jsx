92d7de02fdf9effaf761b2f42a52786ea95c3fb4
serhii.sakal <serhii.sakal@axoni.com>
2021-02-09 15:07:36 -0500

    decompose slider / switcher
    Signed-off-by: serhii.sakal <serhii.sakal@axoni.com>

--- example/src/App/containers/SquigglyGlitch.example/Props.container.jsx
+++ example/src/App/containers/SquigglyGlitch.example/Props.container.jsx
@@ -1,15 +1,16 @@
 /* eslint react/prop-types: 0 */
 import React, { memo } from 'react';
 
-import Slider from '@material-ui/core/Slider';
 import Accordion from '@material-ui/core/Accordion';
 import AccordionDetails from '@material-ui/core/AccordionDetails';
 import AccordionSummary from '@material-ui/core/AccordionSummary';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import Switch from '@material-ui/core/Switch';
 import Typography from '@material-ui/core/Typography';
 
 import useStyles from '../../hooks/useStyles';
+
+import SliderProp from '../../components/Slider.prop';
+import SwitchProp from '../../components/Switch.prop';
 
 const PropsContainer = ({
   title,
@@ -33,91 +34,45 @@ const PropsContainer = ({
       </AccordionSummary>
       <AccordionDetails>
         <div className={classes.fullWidth}>
-          <fieldset className={classes.fieldset}>
-            <Typography component="legend" color="textSecondary" variant="body2">
-              {`duration: ${duration}ms`}
-            </Typography>
+          <SliderProp
+            max={1000}
+            setValue={setDuration}
+            title="duration (ms)"
+            value={duration}
+          >
             <Typography variant="caption">
               note: the higher duration the smallest animation speed
             </Typography>
-            <Slider
-              color="secondary"
-              marks
-              max={1000}
-              min={0}
-              onChange={setDuration}
-              step={10}
-              value={duration}
-              valueLabelDisplay="auto"
-            />
-          </fieldset>
+          </SliderProp>
 
-          <fieldset className={classes.fieldset}>
-            <Typography component="legend" color="textSecondary" variant="body2">
-              {`baseFrequency: ${baseFrequency}`}
-            </Typography>
-            <Slider
-              color="secondary"
-              marks
-              max={1}
-              min={0}
-              onChange={setBaseFrequency}
-              step={0.01}
-              value={baseFrequency}
-              valueLabelDisplay="auto"
-            />
-          </fieldset>
-
-          <fieldset className={classes.fieldset}>
-            <Typography component="legend" color="textSecondary" variant="body2">
-              {`scaleNoise: ${scaleNoise}`}
-            </Typography>
-            <Slider
-              color="secondary"
-              marks
-              max={30}
-              min={-30}
-              onChange={setScaleNoise}
-              step={1}
-              value={scaleNoise}
-              valueLabelDisplay="auto"
-            />
-          </fieldset>
+          <SliderProp
+            max={1}
+            min={0}
+            setValue={setBaseFrequency}
+            step={0.01}
+            title="baseFrequency"
+            value={baseFrequency}
+          />
+          <SliderProp
+            max={30}
+            min={-30}
+            setValue={setScaleNoise}
+            step={1}
+            title="scaleNoise"
+            value={scaleNoise}
+          />
 
           <div className={classes.row}>
-            <fieldset className={classes.fieldset}>
-              <Typography component="legend" color="textSecondary" variant="body2">
-                disabled
-              </Typography>
-              <div className={classes.row}>
-                <Switch
-                  checked={disabled}
-                  color="secondary"
-                  onChange={setDisabled}
-                  size="small"
-                />
-                <Typography color={disabled ? 'secondary' : 'textSecondary'}>
-                  {String(disabled)}
-                </Typography>
-              </div>
-
-            </fieldset>
-            <fieldset className={classes.fieldset}>
-              <Typography component="legend" color="textSecondary" variant="body2">
-                onHover
-              </Typography>
-              <div className={classes.row}>
-                <Switch
-                  checked={onHover}
-                  color="secondary"
-                  onChange={setOnHove}
-                  size="small"
-                />
-                <Typography color={onHover ? 'secondary' : 'textSecondary'}>
-                  {String(onHover)}
-                </Typography>
-              </div>
-            </fieldset>
+            <SwitchProp
+              setValue={setDisabled}
+              title="disabled"
+              value={disabled}
+            />
+            <SwitchProp
+              setValue={setOnHove}
+              title="onHover"
+              value={onHover}
+            />
           </div>
         </div>
       </AccordionDetails>
